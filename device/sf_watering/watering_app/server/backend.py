73067ca11b69fdfc0876dc5152e746646c863b03
Evyatar <evyatar.v@gmail.com>
2026-04-07 12:44:04 +0300

    Fix cron schedule SEC string not to run every sec

--- device/sf_watering/watering_app/server/backend.py
+++ device/sf_watering/watering_app/server/backend.py
@@ -65,8 +65,8 @@ def convert_to_cron(time_str, duration_minutes, days_array):
     total_stop_minutes = (total_start_minutes + duration_minutes) % (24 * 60)
     stop_h = total_stop_minutes // 60
     stop_m = total_stop_minutes % 60
-    start_cron = f"* {start_m} {start_h} * * {cron_days}"
-    stop_cron = f"* {stop_m} {stop_h} * * {cron_days}"
+    start_cron = f"0 {start_m} {start_h} * * {cron_days}"
+    stop_cron = f"0 {stop_m} {stop_h} * * {cron_days}"
     return start_cron, stop_cron
 
 # --- API ENDPOINT: GET ALL SCHEDULES ---
